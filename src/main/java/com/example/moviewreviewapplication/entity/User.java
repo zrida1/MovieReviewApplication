65fb7a95706b94f312fd5a9305a2f574a40be553
zrida1 <ragimovazrida@gmail.com>
2026-07-19 00:33:55 +0400

    CHECKPOINT1_Projectstructure_entitydesign

---
+++ src/main/java/com/example/moviewreviewapplication/entity/User.java
@@ -0,0 +1,19 @@
+package com.example.moviewreviewapplication.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    @OneToMany(mappedBy = "user")
+    private List<Review> reviews;
+}
