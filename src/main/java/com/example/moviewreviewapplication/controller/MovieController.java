d70dca7e526a9732fa7046b84026d44a14888e11
zrida1 <ragimovazrida@gmail.com>
2026-07-21 14:40:58 +0400

    CHECKPOINT6_API_documentation

--- src/main/java/com/example/moviewreviewapplication/controller/MovieController.java
+++ src/main/java/com/example/moviewreviewapplication/controller/MovieController.java
@@ -3,6 +3,8 @@ package com.example.moviewreviewapplication.controller;
 import com.example.moviewreviewapplication.dto.MovieRequestDTO;
 import com.example.moviewreviewapplication.dto.MovieResponseDTO;
 import com.example.moviewreviewapplication.service.MovieService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import org.springframework.data.domain.Page;
 
+@Tag(name = "Movies", description = "Movie Management APIs")
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
@@ -18,21 +21,26 @@ public class MovieController {
         this.movieService = movieService;
     }
 
+    @Operation(summary = "Get all movies")
     @GetMapping
     public Page<MovieResponseDTO> getMovies(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "5") int size, @RequestParam(defaultValue = "title") String sortBy) {
         return movieService.getAllMovies(page, size, sortBy);
     }
 
+    @Operation(summary = "Get movie by id")
     @GetMapping("/{id}")
     public MovieResponseDTO getMovie(@PathVariable Long id) {
         return movieService.getMovieById(id);
     }
 
+
+    @Operation(summary = "Update movie")
     @PutMapping("/{id}")
     public MovieResponseDTO updateMovie(@PathVariable Long id, @Valid @RequestBody MovieRequestDTO movie) {
         return movieService.updateMovie(id, movie);
     }
 
+    @Operation(summary = "Delete movie")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteMovie(@PathVariable Long id) {
@@ -40,6 +48,7 @@ public class MovieController {
 
     }
 
+    @Operation(summary = "Create new movie")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public MovieResponseDTO createNewMovie(@Valid @RequestBody MovieRequestDTO movie) {
--- src/main/java/com/example/moviewreviewapplication/controller/UserController.java
+++ src/main/java/com/example/moviewreviewapplication/controller/UserController.java
@@ -4,13 +4,14 @@ package com.example.moviewreviewapplication.controller;
 import com.example.moviewreviewapplication.dto.UserRequestDTO;
 import com.example.moviewreviewapplication.dto.UserResponseDTO;
 import com.example.moviewreviewapplication.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@Tag(name = "Users", description = "User Management APIs")
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -19,21 +20,25 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Get all users")
     @GetMapping
     public Page<UserResponseDTO> getAllUsers(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "5") Integer size, @RequestParam(defaultValue = "name") String sortBy) {
         return userService.getAllUsers(page, size, sortBy);
     }
 
+    @Operation(summary = "Get user by id")
     @GetMapping("/{id}")
     public UserResponseDTO getUser(@PathVariable Long id) {
         return userService.getUser(id);
     }
 
+    @Operation(summary = "Update user")
     @PutMapping("/{id}")
     public UserResponseDTO updateUser(@PathVariable Long id, @Valid @RequestBody UserRequestDTO user) {
         return userService.updateUser(id, user);
     }
 
+    @Operation(summary = "Delete user")
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable Long id) {
@@ -41,6 +46,7 @@ public class UserController {
 
     }
 
+    @Operation(summary = "Create new user")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public UserResponseDTO createUser(@Valid @RequestBody UserRequestDTO user) {
